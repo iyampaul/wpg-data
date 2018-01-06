60b93800da9cd388ad15a15d5d3adb7bce415036
Paul Harrison <paul@goneplaid.net>
2018-01-06 07:17:21 -0600

    Redesign

---
+++ lib/api_data.py
@@ -0,0 +1,49 @@
+"""
+Pull lane closure data from data.winnipeg.ca
+https://data.winnipeg.ca/browse
+"""
+
+from datetime import datetime
+from sodapy import Socrata
+
+SOCRATA_API = "data.winnipeg.ca"
+
+class API():
+    """ Connect, gather, return lane data  """
+
+    def __init__(self):
+        """ Initialize the OpenData API """
+        self.api = Socrata(SOCRATA_API, None)
+        self.closed_lanes = []
+
+    def get_data(self):
+        """
+        Returned closed lanes on current date
+        """
+        lane_data = self.api.get("h367-iifg")
+
+        for i in lane_data:
+            c_from, c_to = self.closed_dates(i)
+            if self.range_check(c_from, c_to):
+                self.closed_lanes.append(i)
+
+        return self.closed_lanes
+
+    def closed_dates(self, lane):
+        """
+        In: One lane dict (2018-04-28T00:00:00)
+        Out: formatted c_from, c_to datetime variables
+        """
+        c_from = datetime.strptime(lane['date_closed_from'], '%Y-%m-%dT%H:%M:%S')
+        c_to = datetime.strptime(lane['date_closed_to'], '%Y-%m-%dT%H:%M:%S')
+        return c_from, c_to
+
+    def range_check(self, c_from, c_to):
+        """
+        In: closed from, closed to datetime variables
+        Out: binary if today() within closed to/from range.
+        """
+        if c_from <= datetime.today() <= c_to:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,67 +1,17 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 """
-Pull data from data.winnipeg.ca
-https://data.winnipeg.ca/browse
+Reports on lane closures in Winnipeg
 """
 
-from datetime import datetime
-from sodapy import Socrata
+from lib.api_data import API
 
 def __init__():
+    """ Make script go now! """
+    lane_request = API()
 
-    wpg_api = Socrata("data.winnipeg.ca", None)
+    lane_request.get_data()
 
-    lane_list, validated_lanes = date_scope(pull_closures(wpg_api))
-
-    output_lanes(lane_list, validated_lanes)
-
-def pull_closures(wpg_api):
-    """ Pull all publishes lane closures """
-    return wpg_api.get("h367-iifg")
-
-def date_scope(lanes):
-    """
-    In: Full lane output from API
-    Out: lanes closed on current date and original list
-    """
-    validated_lanes = []
-
-    for lane in lanes:
-        # validate the lane
-        closed_from, closed_to = range_format(lane)
-        if date_range_check(closed_from, closed_to):
-            validated_lanes.append(lane['lane_closure_id'])
-
-    return lanes, validated_lanes
-
-def range_format(lane):
-    """
-    In: One lane dict (2018-04-28T00:00:00)
-    Out: formatted closed_from, closed_to strings (YYMMDD)
-    """
-    closed_from = datetime.strptime(lane['date_closed_from'], '%Y-%m-%dT%H:%M:%S')
-    closed_to = datetime.strptime(lane['date_closed_to'], '%Y-%m-%dT%H:%M:%S')
-    return closed_from, closed_to
-
-def date_range_check(closed_from, closed_to):
-    """
-    In: closed_from and closed_to of the lane
-    Out: binary if current date in range
-    """
-    if closed_from <= datetime.today() <= closed_to:
-        return True
-    else:
-        return False
-
-def output_lanes(lane_list, validated_lanes):
-    """ Prints validated lanes """
-    for v_lane in validated_lanes:
-        for lane in lane_list:
-            if lane['lane_closure_id'] == v_lane:
-                print(lane['primary_street'] + " between " + lane['boundaries'])
-                print(lane['traffic_effect'])
-                print("-"*20)
 
 if __name__ == '__main__':
     __init__()
